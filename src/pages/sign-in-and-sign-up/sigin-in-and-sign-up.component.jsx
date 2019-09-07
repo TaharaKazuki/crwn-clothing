6ea68f7277f4b6468f3f7434556939d004e776cf
田原 一樹 <ssp0727.lnc@gmail.com>
2019-09-07 18:46:01 +0900

    [update]

--- src/pages/sign-in-and-sign-up/sigin-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sigin-in-and-sign-up.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import SignIn from '../../components/sign-in/sign-in.component'
-import './sigin-in-and-sign-up.style.scss'
+import './sign-in-and-sign-up.style.scss'
+
+
 
 const SignInAndSignUpPage = () => (
-  <div className="sign-in-and-sign-up">
+  <div className='sign-in-and-sign-up'>
     <SignIn />
   </div>
 )
