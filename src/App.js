4e2c0e64d9b4aefb365b4305f951d02e82a403fc
田原 一樹 <ssp0727.lnc@gmail.com>
2019-09-07 22:12:52 +0900

    [update]

--- src/App.js
+++ src/App.js
@@ -7,20 +7,39 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sigin-in-and-sign-up.component'
 import Header from './components/header/header.component'
+import { auth } from './firebase/firebase.util'
 
 
+class App extends React.Component {
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+  constructor () {
+    super()
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  componentDidMount () {
+    auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user })
+
+      console.info(user)
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
