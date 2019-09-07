c8041f02cf02b821dd5250b99e9f7190a4b7c874
田原 一樹 <ssp0727.lnc@gmail.com>
2019-09-07 19:19:21 +0900

    [update]

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 
 import './custom-button.style.scss'
 
-const CustomButton = ({children, ...otherProps}) => (
-  <button className='custom-button' {...otherProps}>
+const CustomButton = ({ children, ...otherProps }) => (
+  <div className="custom-button" {...otherProps}>
     {children}
-  </button>
+  </div>
 )
-
 export default CustomButton
